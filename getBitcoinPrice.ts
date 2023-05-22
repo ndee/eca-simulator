35640ccf481d5ab722f450a24073ab04d9c60c0e
Andreas Henrich <a.henrich@sap.com>
2023-05-22 16:42:59 +0200

    Get bitcoin price

---
+++ getBitcoinPrice.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export async function getBitcoinPrice(date: Date): Promise<number> {
+    const timestamp = date.getTime();
+    const blocksAroundTimestampResponse = await axios.get(`https://corsproxy.io/?https%3A%2F%2Fblockchain.info%2Fblocks%2F${timestamp}%3Fformat%3Djson`);
+    const blockHeight = blocksAroundTimestampResponse.data[0].height;
+    const priceResponse = await axios.get(`https://tcc.qrsnap.io/block/${blockHeight}/price`);
+    const price = priceResponse.data.result.price;
+    return price;
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { getBitcoinPrice } from "./getBitcoinPrice";
+
+
+const day = new Date(2021, 9, 3);
+getBitcoinPrice(day).then((price) => {
+    console.log(`The price of Bitcoin at ${day.toDateString()} was: ${price} USD`);
+});
